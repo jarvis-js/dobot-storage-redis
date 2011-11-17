4e1f430394544700040ec26b9c7182f4394a067b
Alex Whitman <alex@alexwhitman.com>
2011-11-17 16:44:19 +0000

    Add redis storage module

---
+++ index.js
@@ -0,0 +1,51 @@
+var redis = require('redis');
+
+module.exports = RedisStorage;
+
+function RedisStorage(options) {
+	var defaults = {
+		hostname: 'localhost',
+		port: 6379,
+		password: null,
+		key_prefix: 'dobot:storage:'
+	};
+
+	this.options = defaults;
+	for (var key in options) {
+		this.options[key] = options[key];
+	}
+
+	this.connection = null;
+}
+
+RedisStorage.prototype.init = function(callback) {
+	this.connection = redis.createClient(this.options.port, this.options.hostname);
+	if (this.options.password) {
+		this.connection.auth(this.options.password);
+	}
+	if (callback) {
+		callback();
+	}
+}
+
+RedisStorage.prototype.load = function(key, callback) {
+	this.connection.get(this.options.key_prefix + key, function(error, data) {
+		var loaded = [];
+		if (!error) {
+			try {
+				loaded = JSON.parse(data);
+			}
+			catch (e) {
+				// Not the end of the world.
+			}
+		}
+		callback(loaded);
+	});
+}
+
+RedisStorage.prototype.save = function(key, data, callback) {
+	this.connection.set(this.options.key_prefix + key, JSON.stringify(data));
+	if (callback) {
+		callback();
+	}
+}
